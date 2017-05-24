2d967df0d51ed49549abfd7f1cd9b39df7a7f3a3
ssslb <283531562@qq.com>
2017-05-24 23:31:39 +0800

    fix

---
+++ resource1/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+	<div id="app">
+		<img src="./assets/logo.png">
+		<h1></h1>
+		<h1>Easy-Electron</h1>
+		<h2>快速构建Electron+vue的CLI</h2>
+	</div>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				msg: 'Welcome to Your Easy-Electron'
+			}
+		},
+		created() {
+			console.log(this.$ipc)
+		}
+	}
+</script>
+
+<style lang="scss">
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+	img{
+		width: 300px;
+	}
+	h1, h2 {
+		font-weight: normal;
+	}
+</style>
---
+++ resource1/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import App from './App.vue';
+import Router from './router';
+import Store from './store';
+import ElementUI from 'element-ui';
+import 'normalize.css';
+
+Vue.use(ElementUI);
+Vue.prototype.$ipc = global.ipcRenderer || {};
+Vue.prototype.$remoteApi = global.remoteApi;
+
+new Vue({
+	el: '#app',	
+	Router,
+	Store,
+	render: h => h(App)
+});
